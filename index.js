8305bf88c49a9d42f4e480f68dae06c2f5949ed1
Paola_G <perfectblue@live.com>
2023-05-15 13:20:27 -0700

    added questions for questions array in index.js file

--- index.js
+++ index.js
@@ -11,13 +11,40 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'title',
-        message: 'title'
+        name: 'description',
+        message: 'Describe the what, why, and how of this project'
     },
     {
-        
+        type: 'input',
+        name: 'installation',
+        message: 'Explain the steps required to install the project'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Share the guidelines for how to contribute to this project'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run tests for this project'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license applicable to this project',
+        choices: ["MIT", "Apache License 2.0", "Boost Software License 1.0", "Eclipse Public License 2.0", "none"],
     }
-    //etc
 ];
 
 // TODO: Create a function to write README file
