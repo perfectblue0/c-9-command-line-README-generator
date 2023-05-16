3187677f1912c59e3826ff4c82cd63f85a03f4de
Paola_G <perfectblue@live.com>
2023-05-16 13:43:33 -0700

    added license function that will return link or empy string, depending on whether a license or 'none' was selected

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,14 +3,18 @@ function renderLicenseBadge(license) {
   if (license !== "none") {
     return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
   }
+  // If there is no license, return an empty string
   return "";
 }
-// If there is no license, return an empty string
-
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `\n[License](#license)\n`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
