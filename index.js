75663f4d56a6a6249c664103dc618dff592641a9
Paola_G <perfectblue@live.com>
2023-05-16 13:17:08 -0700

    fixed bug in init function

--- index.js
+++ index.js
@@ -64,10 +64,12 @@ function writeToFile(fileName, data) {
 }
     
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
     inquirer.prompt(questions).then(function(answers) {
         const markdownText = generateMarkdown(answers);
         writeToFile("./dist/README.md", markdownText);
-    })
+    });
+}
+
 // Function call to initialize app
 init();
